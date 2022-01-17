e51d77f0cf8358a5f703eb9e47509bd4757990ac
Ben Smith <ben.f.smith07@gmail.com>
2022-01-17 10:45:14 -0500

    function_exercises.py

---
+++ function_exercises.py
@@ -0,0 +1,13 @@
+# 1. Define a function named is_two. It should accept one input and return 
+#    True if the passed input is either the number or the string 2, 
+#    False otherwise.
+
+def is_two(num):
+    if num in [2, '2']:
+        return True
+    else:
+        return False
+
+# 2. Define a function named is_vowel. It should return True if the passed 
+#    string is a vowel, False otherwise.
+
