fd8b3404317deea20a907b443c015530a95dfdf4
Ben Smith <ben.f.smith07@gmail.com>
2022-01-23 06:15:17 -0600

    ...

---
+++ import_exercises.py
@@ -0,0 +1,9 @@
+
+##############################################################################
+# 1b. Within this file, use from to import the calculate_tip function 
+# directly. Call this function with values you choose and print the result.
+
+from function_exercises import calculate_tip
+
+print(calculate_tip(.2, 100))
+
